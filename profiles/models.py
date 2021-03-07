e3c4b60a8c12063b5ed7980ac41939476371b892
SimonOkello <simonokello93@gmail.com>
2021-03-07 17:02:51 +0300

    more code on profile app

--- profiles/models.py
+++ profiles/models.py
@@ -17,6 +17,11 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def get_friends(self):
+        return self.friends.all()
+    def get_friends_no(self):
+        return self.friends.all().count()
+
     def __str__(self):
         return self.user.username
 
